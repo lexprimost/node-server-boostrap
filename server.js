6acaeb8b1de4f5c879b2159a97ce6c48dd2bd528
Primaël Owald Lebrun <priimost@gmail.com>
2020-08-08 17:58:23 +0000

    server implemented

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//dotenv package initialization
+dotenv.config();
+
+const app = express();
+
+//use port defined in .env files or 3000 if not defined in the .env file
+const PORT = process.env.PORT || 3000;
+
+//simple route
+app.get('/', (req, resp, next) => {
+    resp.send('Hello from Node Server ! this is a minimalist nodejs server to get you started')
+} );
+
+//app starts a server and listens on port defined in PORT variable for connections. 
+app.listen(PORT, () => {
+    console.log(`server listenning on port ${PORT}`)
+});
+
+//-USAGE
+//type in your console : 'npm run dev' to run in development mode
+//--OR
+//type in your console : 'npm run start' to run in production mode
